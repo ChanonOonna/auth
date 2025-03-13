f585a63ca97315a1231d2b0c03bab79160d05954
ChanonOonna <Chanon.oo@ku.th>
2025-03-13 11:33:09 +0700

    Updated files

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(
   session({
     secret: "secret",
-    resave: true, //ขออณุญ
+    resave: false,
     saveUninitialized: true,
   }),
 );
@@ -43,88 +43,100 @@ const initMySQL = async () => {
 /* เราจะแก้ไข code ที่อยู่ตรงกลาง */
 
 app.post('/api/register', async (req, res) => {
-  try {
+  try{
     const { email, password } = req.body;
-    const passwordHash = await bcrypt.hash(password, 10);
-    const userData = {
-      email,
-      password: passwordHash
-    }
-   
-    const [results] = await conn.query('INSERT INTO users SET ?', userData);
-    res.json({
-      message: 'Register success',
-      results
-    })
+  const passwordHash = await bcrypt.hash(password,10);
+  const userData = {
+    email,
+    password: passwordHash
+  }
+
+  const [results] = await conn.query('INSERT INTO users SET ?', userData);
+  res.json({ 
+    message: 'Register success',
+    results
+  })
   } catch (error) {
     console.log('error',error);
-    res.json ({
+    res.json({
       message: 'Register failed',
       error
     })
   }
 });
- 
+
 app.post('/api/login', async (req, res) => {
-
-  try {
+  try{
     const { email, password } = req.body;
-  const [results] = await conn.query('SELECT * FROM users WHERE email = ?', [email]);
-  const userData = results[0];
-  const match = await bcrypt.compare(password, userData.password);
-  if (!match) {
-    res.status(400).json({
-      message: 'Email fail wrong email or password'
-    });
-    return false;
-  }
+    const [results] = await conn.query('SELECT * FROM users WHERE email = ?',[email]);
+    const userData = results[0];
+    const match = await bcrypt.compare(password, userData.password);
+    if(!match){
+      res.status(400).json({
+        message: 'login failed wrong email or password'
+      });
+      return false
+    }
 
-  //สร้าง token
-  const token = jwt.sign({ email,role:"admin" }, secret, { expiresIn: '1h' });
+    // //สร้าง token jwt token
+    // const token = jwt.sign({email, role: 'admin'}, secret, { expiresIn: '1h'});
+    // res.cookie('token',token, {
+    //   maxAge: 300000,
+    //   secure: true,
+    //   httpOnly: true,
+    //   sameSite: 'none',
+    // });
+    
+    req.session.userId = userData.id;
+    req.session.user = userData;
+    console.log(req.sessionID)
 
-  res.json({
-    message: 'Login success',
-    token
 
-  })
+    res.json({
+      message: 'login success',
+    })
   } catch (error) {
     console.log('error',error);
-    res.status(401).json ({
-      message: 'Login failed',
+    res.status(401).json({
+      message: 'login failed',
       error
     })
   }
 })
 
 app.get('/api/users', async (req, res) => {
-  try{
-    const authHeader = req.headers.authorization;
-    let authToken =''
-    if (authHeader) {
-      authToken = authHeader.split(' ')[1];
-    }
-    console.log('authToken',authToken);
-    const user = jwt.verify(authToken, secret);
-    console.log('user',user.email);
- 
-    const [checkResults] = await conn.query('SELECT * FROM users WHERE email = ?', [user.email]);
-    if(!checkResults[0]){
-      throw {message: 'not found user'}
-    }
- 
-    const [results] = await conn.query('SELECT * FROM users');
-    res.json({
-      users: results
-    })
-  } catch (error) {
-    console.log('error',error);
-    res.status(403).json ({
+ try{
+  //const authHeader = req.headers.authorization;
+  //const authToken = req.cookies.token;
+  //let authToken = ''
+  //if (authHeader){
+  //  authToken = authHeader.split(' ')[1];
+  //}
+  //console.log('authToken',authToken);
+  //const user = jwt.verify(authToken, secret);
+  //console.log('user',user.email);
+
+  if(!req.session.userId){
+    throw{message:'authentication failed'}
+  }
+  console.log('session',req.session);
+
+  // const [checkResults] = await conn.query('SELECT * FROM users WHERE email = ?',[user.email]);
+  // if(!checkResults[0]){
+  //   throw {message: 'not found user'}
+  // }
+  const[results] = await conn.query('SELECT * FROM users');
+  res.json({
+    users: results
+  })
+ } catch (error) {
+  console.log('error',error);
+    res.status(403).json({
       message: 'Authentication failed',
       error
     })
-  }
+ }
 })
-
 
 // Listen
 app.listen(port, async () => {
